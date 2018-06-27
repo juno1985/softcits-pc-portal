4ddfdfaa4df1d02c3657fdd7ff3cc3e05a2e4e69
Jun <wangzhen_tju@126.com>
2018-06-27 17:36:54 +0800

    使用EventEmitter子组件向父组件发射变量值 (父组件向子组件传值则使用@Input)

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input,EventEmitter,Output } from '@angular/core';
 
 @Component({
   selector: 'app-stars',
@@ -12,6 +12,13 @@ export class StarsComponent implements OnInit {
   private rating: number = 0;
   @Input()
   private readonly: boolean = true;
+  
+  //将评星发射出去,可以让父组件得到值
+  //这里注意,父组件向子组件传值使用属性绑定
+  //子组件向父组件就得用这种@Output发射事件方式,泛型内是发射值的类型
+  //注意!!!此处@Output的变量名必须为上面@Input名+Change
+  @Output()
+  private ratingChange:EventEmitter<number>=new EventEmitter();
   constructor() { }
 
   ngOnInit() {
@@ -30,6 +37,9 @@ export class StarsComponent implements OnInit {
 
       //重新计算总星星数
       this.ngOnInit();
+      
+      //将评星数发射出去,共父组件得到
+      this.ratingChange.emit(this.rating);
     }
   }
 }
